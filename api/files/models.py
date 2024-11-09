e65a8b3acb2959212be95a9a3c675862efef4b66
Phylosius <hei.phylosius@gmail.com>
2024-11-09 09:32:22 +0300

    fix: created model FileInfo

--- api/files/models.py
+++ api/files/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class FileInfo(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    size = models.IntegerField()
+    upload_date = models.DateTimeField(default=timezone.now, editable=False)
