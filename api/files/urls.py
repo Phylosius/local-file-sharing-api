1251d0d3e21cac6f1efb3326cb9ac0ff6623dd17
Phylosius <hei.phylosius@gmail.com>
2024-11-12 14:38:05 +0300

    feat: created router for FileInfo's Serializer

--- api/files/urls.py
+++ api/files/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework import routers
 
 from files import views
+from files.views import FileInfoSet
 
+router = routers.DefaultRouter()
+router.register('files', FileInfoSet)
 urlpatterns = [
     path('test', views.test, name='files-test'),
 ]
