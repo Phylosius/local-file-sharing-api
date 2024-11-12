e05ea3813de3ba51cd43fecd0c8e8cd6ea91bad9
Phylosius <hei.phylosius@gmail.com>
2024-11-12 14:32:36 +0300

    feat: added view for files.serializers.FileInfoSerializers

--- api/files/views.py
+++ api/files/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from files.models import FileInfo
+from files.serializers import FileInfoSerializer
+
+
+class FileInfoSet(viewsets.ModelViewSet):
+
+    queryset = FileInfo.objects.all()
+    serializer_class = FileInfoSerializer
+
 
 def test(request):
     return HttpResponse("test")
