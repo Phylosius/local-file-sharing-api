715b016a72bdf06c4535f201fc25ac11a9dc9988
Phylosius <hei.phylosius@gmail.com>
2024-11-18 14:56:39 +0300

    feat: added accounts.User to admin interface

--- api/accounts/admin.py
+++ api/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import User
+
+
+@admin.register(User)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'date_joined', 'is_active']
+    list_filter = ['username', 'is_active']
+    search_fields = ['username']
